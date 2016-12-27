aceb645e0008250e31814070c10347a1d3c40a18
Michael Bausor <michaelbausor@google.com>
2016-12-27 15:27:43 -0800

    Change LRO namespace to camel case
    - Change LRO namespace to camel case
    - Add license

--- src/OperationResponse.php
+++ src/OperationResponse.php
@@ -1,4 +1,34 @@
 <?php
+/*
+ * Copyright 2016, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace Google\GAX;
 
@@ -31,7 +61,7 @@ class OperationResponse
      * OperationResponse constructor.
      *
      * @param string $operationName
-     * @param \Google\Longrunning\OperationsClient $operationsClient
+     * @param \Google\GAX\LongRunning\OperationsClient $operationsClient
      * @param array $options {
      *                       Optional. Options for configuring the Operation response object.
      *
@@ -198,7 +228,7 @@ class OperationResponse
     }
 
     /**
-     * @return \Google\Longrunning\OperationsClient The OperationsClient object used to make
+     * @return \Google\GAX\LongRunning\OperationsClient The OperationsClient object used to make
      * requests to the operations API.
      */
     public function getOperationsClient()
