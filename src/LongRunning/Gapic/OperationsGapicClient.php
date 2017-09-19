2ab9589b640b6ee65314a836634093a421a555f9
Michael Bausor <michaelbausor@google.com>
2017-09-18 17:02:31 -0700

    Refresh with latest toolkit

--- src/LongRunning/Gapic/OperationsGapicClient.php
+++ src/LongRunning/Gapic/OperationsGapicClient.php
@@ -147,8 +147,8 @@ class OperationsGapicClient
         if (!self::$gapicVersionLoaded) {
             if (file_exists(__DIR__.'/../VERSION')) {
                 self::$gapicVersion = trim(file_get_contents(__DIR__.'/../VERSION'));
-            } elseif (class_exists('\Google\Cloud\ServiceBuilder')) {
-                self::$gapicVersion = \Google\Cloud\ServiceBuilder::VERSION;
+            } elseif (class_exists('\Google\Cloud\Version')) {
+                self::$gapicVersion = \Google\Cloud\Version::VERSION;
             }
             self::$gapicVersionLoaded = true;
         }
