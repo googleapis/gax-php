0d8dbf8e25645d6b8239f6d74dea957ee3747c5f
Brent Shaffer <bshafs@gmail.com>
2017-09-22 15:50:33 -0700

    adds Middlewares

--- src/ApiCallable.php
+++ src/ApiCallable.php
@@ -31,8 +31,11 @@
  */
 namespace Google\GAX;
 
-use Exception;
-use InvalidArgumentException;
+use Google\GAX\Middleware\CustomHeaderMiddleware;
+use Google\GAX\Middleware\LongRunningMiddleware;
+use Google\GAX\Middleware\PageStreamingMiddleware;
+use Google\GAX\Middleware\RetryMiddleware;
+use Google\GAX\Middleware\TimeoutMiddleware;
 
 /**
  * Creates a function wrapper that provides extra functionalities such as retry and bundling.
@@ -42,132 +45,33 @@ class ApiCallable
     const TRANSPORT_METHOD_PARAM_COUNT = 2;
     const TRANSPORT_METHOD_OPTIONS_INDEX = 1;
 
-    private static function setTimeout($apiCall, $timeoutMillis)
+    private static function setTimeout($callable, $timeoutMillis)
     {
-        $inner = function () use ($apiCall, $timeoutMillis) {
-            $params = func_get_args();
-            if (count($params) != self::TRANSPORT_METHOD_PARAM_COUNT ||
-                !is_array($params[self::TRANSPORT_METHOD_OPTIONS_INDEX])
-            ) {
-                throw new InvalidArgumentException('Invalid parameter count or options argument not found.');
-            } else {
-                $params[self::TRANSPORT_METHOD_OPTIONS_INDEX]['timeoutMillis'] = $timeoutMillis;
-            }
-            return call_user_func_array($apiCall, $params);
-        };
-        return $inner;
+        return new TimeoutMiddleware($callable, $timeoutMillis);
     }
 
-    private static function setRetry($apiCall, RetrySettings $retrySettings, $timeFuncMillis)
+    private static function setRetry($callable, RetrySettings $retrySettings, $timeFuncMillis)
     {
-        if (!isset($timeFuncMillis)) {
-            $timeFuncMillis = function () {
-                return microtime(true) / 1000.0;
-            };
-        }
-
-        $inner = function () use ($apiCall, $retrySettings, $timeFuncMillis) {
-
-            // Initialize retry parameters
-            $delayMult = $retrySettings->getRetryDelayMultiplier();
-            $maxDelayMillis = $retrySettings->getMaxRetryDelayMillis();
-            $timeoutMult = $retrySettings->getRpcTimeoutMultiplier();
-            $maxTimeoutMillis = $retrySettings->getMaxRpcTimeoutMillis();
-            $totalTimeoutMillis = $retrySettings->getTotalTimeoutMillis();
-
-            $delayMillis = $retrySettings->getInitialRetryDelayMillis();
-            $timeoutMillis = $retrySettings->getInitialRpcTimeoutMillis();
-            $currentTimeMillis = $timeFuncMillis();
-            $deadlineMillis = $currentTimeMillis + $totalTimeoutMillis;
-
-            while ($currentTimeMillis < $deadlineMillis) {
-                $nextApiCall = self::setTimeout($apiCall, $timeoutMillis);
-                try {
-                    return call_user_func_array($nextApiCall, func_get_args());
-                } catch (ApiException $e) {
-                    if (!in_array($e->getStatus(), $retrySettings->getRetryableCodes())) {
-                        throw $e;
-                    }
-                } catch (Exception $e) {
-                    throw $e;
-                }
-                // Don't sleep if the failure was a timeout
-                if ($e->getStatus() != ApiStatus::DEADLINE_EXCEEDED) {
-                    usleep($delayMillis * 1000);
-                }
-                $currentTimeMillis = $timeFuncMillis();
-                $delayMillis = min($delayMillis * $delayMult, $maxDelayMillis);
-                $timeoutMillis = min(
-                    $timeoutMillis * $timeoutMult,
-                    $maxTimeoutMillis,
-                    $deadlineMillis - $currentTimeMillis
-                );
-            }
-            throw new ApiException(
-                "Retry total timeout exceeded.",
-                \Google\Rpc\Code::DEADLINE_EXCEEDED,
-                ApiStatus::DEADLINE_EXCEEDED
-            );
-        };
-        return $inner;
+        return new RetryMiddleware($callable, $retrySettings, $timeFuncMillis);
     }
 
     private static function setPageStreaming($callable, $pageStreamingDescriptor)
     {
-        $inner = function () use ($callable, $pageStreamingDescriptor) {
-            return new PagedListResponse(func_get_args(), $callable, $pageStreamingDescriptor);
-        };
-        return $inner;
+        return new PageStreamingMiddleware($callable, $pageStreamingDescriptor);
     }
 
     private static function setLongRunnning($callable, $longRunningDescriptor)
     {
-        $inner = function () use ($callable, $longRunningDescriptor) {
-            $response = call_user_func_array($callable, func_get_args());
-            $name = $response->getName();
-            $client = $longRunningDescriptor['operationsClient'];
-            $options = $longRunningDescriptor + [
-                'lastProtoResponse' => $response,
-            ];
-            return new OperationResponse($name, $client, $options);
-        };
-        return $inner;
+        return new LongRunningMiddleware($callable, $longRunningDescriptor);
     }
 
     private static function setCustomHeader($callable, $headerDescriptor, $userHeaders = null)
     {
-        $inner = function () use ($callable, $headerDescriptor, $userHeaders) {
-            $params = func_get_args();
-
-            if (count($params) != self::TRANSPORT_METHOD_PARAM_COUNT ||
-                !is_array($params[self::TRANSPORT_METHOD_OPTIONS_INDEX])
-            ) {
-                throw new InvalidArgumentException('Invalid parameter count or options argument is not found.');
-            } else {
-                $headers = [];
-                // Check user-specified $headers first, and then merge $headerDescriptor headers, to ensure
-                // that $headerDescriptor headers such as x-goog-api-client cannot be overwritten
-                // by the $userHeaders.
-                $options = $params[self::TRANSPORT_METHOD_OPTIONS_INDEX];
-                if (array_key_exists('headers', $options)) {
-                    $headers = $options['headers'];
-                }
-                if (!is_null($userHeaders)) {
-                    $headers = array_merge($headers, $userHeaders);
-                }
-                if (!is_null($headerDescriptor)) {
-                    $headers = array_merge($headers, $headerDescriptor->getHeader());
-                }
-                $params[self::TRANSPORT_METHOD_OPTIONS_INDEX]['headers'] = $headers;
-                return call_user_func_array($callable, $params);
-            }
-        };
-        return $inner;
+        return new CustomHeaderMiddleware($callable, $headerDescriptor, $userHeaders);
     }
 
     /**
-     * @param mixed $stub the transport stub to make calls through.
-     * @param string $methodName the method name on the stub to call.
+     * @param callable $callable a callable to make API call through.
      * @param \Google\GAX\CallSettings $settings the call settings to use for this call.
      * @param array $options {
      *     Optional.
@@ -188,15 +92,15 @@ class ApiCallable
     ) {
         ApiCallable::validateApiCallSettings($settings, $options);
 
-        $apiCall = function() use ($transport, $methodName) {
+        $callable = function() use ($transport, $methodName) {
             $callable = [$transport, $methodName];
             return call_user_func_array($callable, func_get_args());
         };
 
         // Call the sync method "wait" if this is not a gRPC call
         if (!array_key_exists('grpcStreamingDescriptor', $options)) {
-            $apiCall = function() use ($apiCall) {
-                return call_user_func_array($apiCall, func_get_args())->wait();
+            $callable = function() use ($callable) {
+                return call_user_func_array($callable, func_get_args())->wait();
             };
         }
 
@@ -207,25 +111,25 @@ class ApiCallable
                 if (array_key_exists('timeFuncMillis', $options)) {
                     $timeFuncMillis = $options['timeFuncMillis'];
                 }
-                $apiCall = self::setRetry($apiCall, $retrySettings, $timeFuncMillis);
+                $callable = self::setRetry($callable, $retrySettings, $timeFuncMillis);
             } elseif ($retrySettings->getNoRetriesRpcTimeoutMillis() > 0) {
-                $apiCall = self::setTimeout($apiCall, $retrySettings->getNoRetriesRpcTimeoutMillis());
+                $callable = self::setTimeout($callable, $retrySettings->getNoRetriesRpcTimeoutMillis());
             }
         }
 
         if (array_key_exists('pageStreamingDescriptor', $options)) {
-            $apiCall = self::setPageStreaming($apiCall, $options['pageStreamingDescriptor']);
+            $callable = self::setPageStreaming($callable, $options['pageStreamingDescriptor']);
         }
 
         if (array_key_exists('longRunningDescriptor', $options)) {
-            $apiCall = self::setLongRunnning($apiCall, $options['longRunningDescriptor']);
+            $callable = self::setLongRunnning($callable, $options['longRunningDescriptor']);
         }
 
         if (array_key_exists('headerDescriptor', $options) || !is_null($settings->getUserHeaders())) {
-            $apiCall = self::setCustomHeader($apiCall, $options['headerDescriptor'], $settings->getUserHeaders());
+            $callable = self::setCustomHeader($callable, $options['headerDescriptor'], $settings->getUserHeaders());
         }
 
-        return $apiCall;
+        return $callable;
     }
 
     private static function validateApiCallSettings(CallSettings $settings, $options)
