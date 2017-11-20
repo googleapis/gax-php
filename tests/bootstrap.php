c919dea3245ab5d5b6752de26e883da31cccb5c0
Michael Bausor <michaelbausor@google.com>
2017-11-20 09:31:41 -0800

    Update tests settings

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set('UTC');
 ini_set('error_reporting', E_ALL);
