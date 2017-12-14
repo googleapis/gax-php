fd58ed711b32ad20e97fc39ab59a3d29ae5c0646
Dave Supplee <dwsupplee@gmail.com>
2017-12-14 11:41:58 -0500

    update longrunning gapic

---
+++ src/ApiCore/LongRunning/resources/operations_descriptor_config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.longrunning.Operations' => [
+            'listOperations' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getOperations',
+                ],
+            ],
+        ],
+    ],
+];
