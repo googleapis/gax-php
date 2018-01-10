7fe372bfe545f7eb66834e7cccebe7202cfb2176
Brent Shaffer <betterbrent@google.com>
2018-01-09 17:52:45 -0800

    adds additionalBindings to rest config

--- src/ApiCore/Parser.php
+++ src/ApiCore/Parser.php
@@ -67,37 +67,18 @@ class Parser
         Segment::resetSegmentCount();
         Segment::resetBindingCount();
 
-        try {
-            Segment::resetSegmentCount();
-            Segment::resetBindingCount();
-            if ($data === "") {
-                // create an empty string literal segment
-                $segments = [
-                    new Segment(Segment::TERMINAL, "")
-                ];
-            } else {
+        if ($data === "") {
+            // create an empty string literal segment
+            $segments = [
+                new Segment(Segment::TERMINAL, "")
+            ];
+        } else {
+            try {
                 $segments = $this->parser->parse($data);
+            } catch (Exception $e) {
+                throw new ValidationException('Exception in parser', 0, $e);
             }
-            $this->segmentCount = Segment::getSegmentCount();
-            // Validation step: checks that there are no nested bindings.
-            $pathWildcard = false;
-            foreach ($segments as $segment) {
-                if ($segment->kind == Segment::TERMINAL &&
-                        $segment->literal == '**') {
-                    if ($pathWildcard) {
-                        throw new ValidationException(
-                            'validation error: path template cannot contain '.
-                            'more than one path wildcard'
-                        );
-                    }
-                    $pathWildcard = true;
-                }
-            }
-            return $segments;
-        } catch (Exception $e) {
-            throw new ValidationException('Exception in parser', 0, $e);
         }
-
         $this->segmentCount = Segment::getSegmentCount();
         // Validation step: checks that there are no nested bindings.
         $pathWildcard = false;
--- tests/ApiCore/Tests/Unit/testdata/test_service_rest_client_config.php
+++ tests/ApiCore/Tests/Unit/testdata/test_service_rest_client_config.php
@@ -87,6 +87,36 @@ return [
                             'getName',
                         ],
                     ]
+                ]
+            ],
+            'MethodWithAdditionalBindings' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{name=message/**}/additional/bindings',
+                'body' => '*',
+                'additionalBindings' => [
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v2/{nested_message=nested/**}/additional/bindings',
+                        'body' => '*'
+                    ],
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v1/{name=different/format/**}/additional/bindings',
+                        'body' => '*'
+                    ],
+                ],
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName'
+                        ]
+                    ],
+                    'nested_message' => [
+                        'getters' => [
+                            'getNestedMessage',
+                            'getName'
+                        ]
+                    ]
                 ],
             ],
         ],
